84be58e71bede00520c75b03d59912eb3f72cfc3
Ubuntu <ubuntu@ip-172-31-3-7.ap-southeast-2.compute.internal>
2019-08-20 11:42:30 +0000

    added request call

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 
 
+var request = require("request");
 
+request("http://ec2-13-239-37-120.ap-southeast-2.compute.amazonaws.com:9000/api/subjects", function(error, response, body){
+	console.log("statuscode: ", response && response.statusCode);
+	console.log("body: ", body);
+});
 
 
 
