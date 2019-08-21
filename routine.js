18a116ba7e34da25209c84b0cdcfe836cb66f7c0
Ubuntu <ubuntu@ip-172-31-3-7.ap-southeast-2.compute.internal>
2019-08-21 11:07:56 +0000

    added routine

---
+++ routine.js
@@ -0,0 +1,29 @@
+
+
+var entities = [{ id: "3k2l12ek", pwd: "ddd"}, {id: "lk3jkl2", pwd: "ggg"}];
+
+var tabbles = ["bills", "spend", "receive", "invoice"];
+
+
+
+entities.forEach( function(val, ind, array){
+	console.log("id: " + val.id);
+	console.log("pwd: " + val.pwd);
+
+})
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 
 var request = require("request");
 
-request("http://ec2-13-239-37-120.ap-southeast-2.compute.amazonaws.com:9000/api/subjects", function(error, response, body){
+var endpoints = ["subjects", "documents"];
+
+
+
+
+if( endpoints.includes(process.argv[2]) ){
+
+  request("http://ec2-13-239-37-120.ap-southeast-2.compute.amazonaws.com:9000/api/subjects", function(error, response, body){
 	console.log("statuscode: ", response && response.statusCode);
 	console.log("body: ", body);
-});
+  });
+
+
+}
 
 console.log("arguments are");
 process.argv.forEach(function(val){
