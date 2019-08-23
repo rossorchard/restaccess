9f641a9474930017b3e43d9acae18ff78d1dc360
Ubuntu <ubuntu@ip-172-31-3-7.ap-southeast-2.compute.internal>
2019-08-23 07:34:54 +0000

    add first part of endpoint object

--- routine.js
+++ routine.js
@@ -5,14 +5,25 @@ var entities = [{ id: "3k2l12ek", pwd: "ddd"}, {id: "lk3jkl2", pwd: "ggg"}];
 var tabbles = ["bills", "spend", "receive", "invoice"];
 
 
+function endpoint( entityId, entityPwd ){
+	var retval = { fetch: function(callback){
+		callback({ name: "test", age:33}, null);
+	}};
+	return retval;
+}
 
+
+
+
+// iterate through entities array and extract information from endpoint for each entry
 entities.forEach( function(val, ind, array){
 	console.log("id: " + val.id);
 	console.log("pwd: " + val.pwd);
+	endpoint(val.id, val.pwd).fetch(function(data, error){
+		console.log(data);
+	});
 
-})
-
-;
+});
 
 
 
